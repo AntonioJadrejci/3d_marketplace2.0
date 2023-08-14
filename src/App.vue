573b1976895ca373bfe8705b9c71e3aeba1f7929
AntonioJadrejci <ajadrejcic@unipu.hr>
2023-08-14 18:57:39 +0200

    Search bar

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,22 @@
 <template>
   <div id="app">
     <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/Login">Prijava</router-link>
-      <router-link to="/Signup">Registracija</router-link>
+      <form class="navbar-form navbar-left" role="search">
+        <div class="form-group">
+          <input type="text" class="form-control" placeholder="Pretraživanje" />
+        </div>
+      </form>
+
+      <router-link style="text decoration: none" to="/"
+        >3D MARKETPLACE</router-link
+      >
+      |
+      <router-link style="text decoration: none" to="/Login"
+        >Prijava</router-link
+      >
+      <router-link style="text decoration: none" to="/Signup"
+        >Registracija</router-link
+      >
     </nav>
     <router-view />
   </div>
@@ -14,7 +27,7 @@
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+
   color: #2c3e50;
   background-color: #031413;
 }
