5e5a305946543fd98d31fbca3b33aa8bebbb0d83
AntonioJadrejci <ajadrejcic@unipu.hr>
2023-08-17 12:39:50 +0200

    Logo

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,15 @@
 <template>
   <div id="app">
     <nav>
+      <div class="home">
+        <img
+          alt="Pozadinska slika"
+          class="Background"
+          src="./assets/Naslov.png"
+          width="200px"
+        />
+      </div>
+
       <form class="navbar-form navbar-left" role="search">
         <div class="form-group">
           <input type="text" class="form-control" placeholder="Pretraživanje" />
@@ -17,6 +26,7 @@
       <router-link style="text decoration: none" to="/Signup"
         >Registracija</router-link
       >
+      <router-link style="text decoration: none" to="/Opis">Opis</router-link>
     </nav>
     <router-view />
   </div>
@@ -29,7 +39,7 @@
   -moz-osx-font-smoothing: grayscale;
 
   color: #2c3e50;
-  background-color: #031413;
+  background-color: #000000;
 }
 
 nav {
@@ -39,6 +49,7 @@ nav {
     font-weight: bold;
     color: #34f1f3;
     font-size: 20px;
+    margin-right: 20px;
 
     &.router-link-exact-active {
       color: #34f1f3;
---
+++ src/views/Opis.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <p2>
+      Ovo je 3D marketplace, mjesto na kojemu možete pronaći veliki broj 3d
+      modela koji su kompatibilni u programu blender. Isto tako možete i vi sami
+      učiniti 3d marketplace boljim za upotrebu tako da objavite vaše vlastite
+      3d modele koji će pomoći drugim korisnicima.
+    </p2>
+    <div class="home">
+      <img
+        alt="Pozadinska slika"
+        class="Background"
+        src="../assets/Background.png"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "HomeView",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
+<style scoped>
+.Background {
+  position: relative;
+  width: 100%;
+  top: 0;
+  left: 0;
+}
+</style>
