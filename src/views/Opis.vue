f1088363e6d9f111204c77b8df1d54591a04a39b
AntonioJadrejci <ajadrejcic@unipu.hr>
2023-08-18 11:47:10 +0200

    Opis - font size

--- src/views/Opis.vue
+++ src/views/Opis.vue
@@ -1,11 +1,13 @@
 <template>
   <div>
-    <p2>
-      Ovo je 3D marketplace, mjesto na kojemu možete pronaći veliki broj 3d
-      modela koji su kompatibilni u programu blender. Isto tako možete i vi sami
-      učiniti 3d marketplace boljim za upotrebu tako da objavite vaše vlastite
-      3d modele koji će pomoći drugim korisnicima.
-    </p2>
+    <div class="descr">
+      <p2>
+        Ovo je 3D marketplace, mjesto na kojemu možete pronaći veliki broj 3d
+        modela koji su kompatibilni u programu blender. Isto tako možete i vi
+        sami učiniti 3d marketplace boljim za upotrebu tako da objavite vaše
+        vlastite 3d modele koji će pomoći drugim korisnicima.
+      </p2>
+    </div>
     <div class="home">
       <img
         alt="Pozadinska slika"
@@ -33,5 +35,11 @@ export default {
   width: 100%;
   top: 0;
   left: 0;
+}
+.descr {
+  font-size: 40px;
+  text-align: center;
+  padding: 25px 0;
+  margin-bottom: 25px;
 }
 </style>
