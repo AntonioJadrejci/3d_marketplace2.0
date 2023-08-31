a8ff2bc43649c4ec802bfb5086608394fda41c07
AntonioJadrejci <ajadrejcic@unipu.hr>
2023-08-31 12:29:07 +0200

    Stvaranje preostalih vue kartica

---
+++ src/views/ModelPage.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="home-background"></div>
+</template>
+
+<script>
+export default {
+  name: "model-page",
+  components: {},
+};
+</script>
+
+<style scoped>
+.home-background {
+  background-color: #95f9ffa6;
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  width: 100%;
+  position: relative;
+}
+
+.header {
+  background-color: #3bd5ea;
+  color: #fff;
+  font-size: 2em;
+  text-align: left;
+  padding: 10px 6px;
+  flex: 0 0 auto;
+}
+</style>
---
+++ src/views/ProfilView.vue
@@ -0,0 +1,28 @@
+<template></template>
+
+<script>
+export default {
+  name: "profili",
+  components: {},
+};
+</script>
+
+<style scoped>
+.home-background {
+  background-color: #95f9ffa6;
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  width: 100%;
+  position: relative;
+}
+
+.header {
+  background-color: #3bd5ea;
+  color: #fff;
+  font-size: 2em;
+  text-align: left;
+  padding: 10px 6px;
+  flex: 0 0 auto;
+}
+</style>
