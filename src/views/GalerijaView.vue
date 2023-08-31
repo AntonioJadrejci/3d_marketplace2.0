70dfc94c37ac6c906b885949672dea4a8794ef65
AntonioJadrejci <ajadrejcic@unipu.hr>
2023-08-31 12:08:14 +0200

    galerija css

--- src/views/GalerijaView.vue
+++ src/views/GalerijaView.vue
@@ -1,4 +1,23 @@
-<template></template>
+<template>
+  <div class="home-background">
+    <div class="panel">
+      <div class="header">
+        Galerija
+        <hr style="border: 2px solid rgb(255, 255, 255)" />
+
+        <v-select
+          v-model="selected_blender_version"
+          :items="blender_versions"
+          label="Blender verzija"
+          small
+          class="ml-0 mr-auto"
+          style="width: 200px"
+        >
+        </v-select>
+      </div>
+    </div>
+  </div>
+</template>
 
 <script>
 export default {
@@ -8,4 +27,21 @@ export default {
 </script>
 
 <style scoped>
+.home-background {
+  background-color: #95f9ffa6;
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  width: 100%;
+  position: relative;
+}
+
+.header {
+  background-color: #3bd5ea;
+  color: #fff;
+  font-size: 2em;
+  text-align: left;
+  padding: 10px 6px;
+  flex: 0 0 auto;
+}
 </style>
